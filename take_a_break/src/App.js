011013cfadd3de875d35e84bd109c86774e9dbfd
Alison Manning <alisonlaurenmanning@gmail.com>
2021-02-11 14:17:43 -0600

    changed in h1's

--- take_a_break/src/App.js
+++ take_a_break/src/App.js
@@ -4,8 +4,8 @@ import QuoteList from './components/QuoteList/QuoteList'
 function App() {
     return (
         <div>
-            <h1>Need A Break From Coding?</h1>
-            <h2>Here's the site to alleviate your brain from all frustration.</h2>
+            <h1>Kanye West:</h1>
+            <h2>An overflowing well of wisdom</h2>
             
             <QuoteList />
         </div>
